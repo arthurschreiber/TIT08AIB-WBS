c01945763123174f109565fdbf7320d46c6e19bc
Arthur Schreiber <schreiber.arthur@gmail.com>
2011-04-12 00:05:23 +0200

    Small spec addition.

--- ruby/spec/version_space_spec.rb
+++ ruby/spec/version_space_spec.rb
@@ -51,6 +51,7 @@ describe VersionSpace do
     it "should return true if the first list is equal to the second" do
       @vs.more_general?(["a"], ["a"]).should be_true
       @vs.more_general?([:*, "b"], ["a", "b"]).should be_true
+      @vs.more_general?(["<18", :*, :*, :*, :*, :*, :*], ["19-24", "w", "nein", "0", "keiner", "Selbstaendig", "3000-3999"]).should be_false
     end
   end
 
