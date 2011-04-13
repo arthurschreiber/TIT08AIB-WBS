e543623bc9bbd5199023a4e7b3c43e6c33a01e6f
Arthur Schreiber <schreiber.arthur@gmail.com>
2011-04-13 21:03:41 +0200

    Change the Clojure Code Structure a bit and add comments.

--- clojure/src/version_space.clj
+++ clojure/src/version_space.clj
@@ -1,17 +1,16 @@
 (ns version-space)
 
+;; Erstellt eine neue Hypothese, die genereller als die beiden
+;; übergebenen Hypothesen ist.
 (defn generalize [hyp1 hyp2]
-  (map
-    (fn [a b]
-      (cond
-        (= a :_) b
-        (= b :_) a
-        (= a b) a
-        true :*
-      )
+  (map (fn [a b]
+    (cond
+      (= a :_) b
+      (= b :_) a
+      (= a b) a
+      true :*
     )
-    hyp1 hyp2
-  )
+  ) hyp1 hyp2)
 )
 
 (defn includes? [a b]
@@ -22,21 +21,21 @@
   (every? true? (map includes? a b))
 )
 
+;; Gibt eine Liste von Positionen zurück, an denen `g`
+;; spezialisiert werden kann.
 (defn get-potential-positions [g neg s]
-  (keep-indexed
-    (fn [index potential] (if (true? potential) index) )
-    (map
-      (fn [g_item neg_item s_item]
-        (and
-          (= g_item :*)
-          (not (= s_item neg_item))
-        )
+  (keep-indexed (fn [index potential]
+    (if (true? potential) index)
+  ) (map (fn [g_item neg_item s_item]
+      (and
+        (= g_item :*)
+        (not (= s_item neg_item))
       )
-      g neg s
-    )
+    ) g neg s)
   )
 )
 
+;; Spezialisiert `g` an der Position `pos`
 (defn specialize-at-position [g s pos]
   (if (= pos 0)
     (cons (first s) (rest g))
@@ -44,6 +43,8 @@
   )
 )
 
+;; Erstellt eine Liste von Spezialisierungen der Hypothese `g`
+;; an Hand des Negativ-Beispieles `neg` und der speziellen Hypothese `s`
 (defn specialize [g neg s]
   (map 
     (fn [pos] (specialize-at-position g s pos) )
@@ -51,71 +52,77 @@
   )
 )
 
+;; Der Versionsraum `vs` wird mit Hilfe des Positiv-Beispiels
+;; `example` verfeinert
 (defn positive-example [vs example]
   (list
-    (filter
-      (fn [g]
-        (more-general? g example)
-      )
-      (first vs)
-    )
+    ;; Lösche alle Elemente aus G, die das Positiv-Beispiel nicht enthalten
+    (filter (fn [g]
+      (more-general? g example)
+    ) (first vs))
     
-    (filter
-      (fn [s]
-        (not-any?
-          (fn [g]
-            (more-general? s g)
-          )
-          (first vs)
-        )
+    ;; Streiche alle Elemente von S, die allgemeiner sind als ein Element von G
+    (filter (fn [s]
+      (not-any? (fn [g]
+        (more-general? s g)
+      ) (first vs))
+    ) (map (fn [s]
+      ;; Ersetze alle Elemente aus S, die das Positiv-Beispiel nicht
+      ;; enthalten durch eine Verallgemeinerung
+      (if
+        (not (more-general? s example))
+        (generalize s example)
+        s
       )
-      (map
-        (fn [s]
-          (if (not (more-general? s example)) (generalize s example) s)
-        )
-        (second vs)
-      )
-    )
+    ) (second vs)) )
   )
 )
 
+;; Der Versionsraum `vs` wird mit dem übergebenen Negativ-Beispiel
+;; `example` verfeinert.
 (defn negative-example [vs example]
   (list
-    (reduce
-      (fn [acc g]
-        (if
-          (not (more-general? g example))
-          (concat acc (list g))
-          (concat acc
-            (reduce
-              (fn [acc new_g]
-                (if
+    (reduce (fn [acc g]
+      (if
+        (not (more-general? g example))
+        (concat acc (list g))
+        (concat acc
+          ;; Generiere alle Spezialisierungen und sammele diejenigen, die alle
+          ;; Kriterien erfüllen.
+          (reduce (fn [acc new_g]
+            (if
+              (and
+                ;; Die Spezialisierung darf das negative Beispiel nicht enthalten
+                (not (more-general? new_g example))
+                ;; Alle bisher vorgelegten Beispiele müssen enthalten sein
+                ;; NOTE: Das stimmt hier eigentlich nicht _direkt_. Wir testn hier
+                ;;       eigentlich gegen die Elemente von S. Da S aber in unserem
+                ;;       Fall dem positiven Beispielen entspricht, passt das soweit.
+                (every? (fn [pe]
+                  more-general? new_g pe
+                ) (second vs))
+                ;; Kein Element von G darf die neue Spezialisierung enthalten,
+                ;; da sie ja sonst überflüssig ist.
+                (not-any? (fn [other_g]
                   (and
-                    (not (more-general? new_g example))
-                    (every? (fn [pe] more-general? new_g pe) (second vs))
-                    (not-any?
-                      (fn [other_g]
-                        (and
-                          (not (= g other_g))
-                          (more-general? other_g new_g)
-                        )
-                      )
-                      (first vs)
-                    )
+                    (not (= g other_g))
+                    (more-general? other_g new_g)
                   )
-                  (concat acc (list new_g))
-                  acc
-                )
+                ) (first vs) )
               )
-             '()
-              (specialize g example (first (second vs)))
+              ;; Die neue Spezialisierung erfüllt alle Kriterien
+              ;; -> wird zum Akkumulator hinzugefügt.
+              (concat acc (list new_g))
+              ;; Die neue Spezialisierung erfüllt die Kriterien nicht
+              ;; -> wird nicht zum Akkumulator hinzugefügt.
+              acc
             )
-          )
+          ) '() (specialize g example (first (second vs))) )
         )
       )
-      '() (first vs)
-    )
+    ) '() (first vs) )
     
+    ;; Alle Elemente aus S entfernen, die dem negativen Beispiel entsprechen
     (filter
       (fn [s]
         (not (more-general? s example))
@@ -125,6 +132,7 @@
   )
 )
 
+;; Generiert eine neue Liste, in der `item` `size` mal vorkommt.
 (defn gen-list [item size]
   (if
     (<= size 0)
@@ -136,6 +144,7 @@
   )
 )
 
+;; Generiert einen neuen, leeren Versionsraum
 (defn gen-version-space [size]
   (list
     (list (gen-list :* size))
