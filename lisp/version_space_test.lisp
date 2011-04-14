3b7ecd99eaf531586d5ee267042551996c3ef99b
Arthur Schreiber <schreiber.arthur@gmail.com>
2011-04-14 16:38:04 +0200

    Change the code style for the tests.

--- lisp/version_space_test.lisp
+++ lisp/version_space_test.lisp
@@ -5,36 +5,23 @@
 
 (define-test test-generalize
   "should return an empty list if passed an empty hypothesis"
-  (assert-equal '()
-    (generalize '() '("a" "b"))
-  )
+  (assert-equal '() (generalize '() '("a" "b")) )
   
   "generalizes the two passed hypotheses"
-  (assert-equal '("a")
-    (generalize '("a") '("a"))
-  )
-      
-  (assert-equal '("a")
-    (generalize '("_") '("a"))
-  )
-  
-  (assert-equal '("a")
-    (generalize '("a") '("_"))
-  )
-    
-  (assert-equal '("*")
-    (generalize '("a") '("b"))
-  )
+  (assert-equal '("a") (generalize '("a") '("a")) )
+  (assert-equal '("a") (generalize '("_") '("a")) )
+  (assert-equal '("a") (generalize '("a") '("_")) )
+  (assert-equal '("*") (generalize '("a") '("b")) )
 )
 
 (define-test test-more-general?
   "should return true if the first list is equal to the second"
-  (assert-true (more-general? '("a") '("a")))
-  (assert-true (not (more-general? '("a") '("b"))))
+  (assert-true (more-general? '("a") '("a")) )
+  (assert-true (not (more-general? '("a") '("b"))) )
 
   "should return true if the first list is more general than the second"
-  (assert-true (more-general? '("a" "*" "c") '("a" "b" "c")))
-  (assert-true (not (more-general? '("a" "*" "c") '("a" "b" "d"))))
+  (assert-true (more-general? '("a" "*" "c") '("a" "b" "c")) )
+  (assert-true (not (more-general? '("a" "*" "c") '("a" "b" "d"))) )
 )
 
 (run-tests)
